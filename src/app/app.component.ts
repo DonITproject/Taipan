d7a3e26f70783c420586eb1a6d4b423b6b2dde4d
Sergey Don <>
2025-01-08 16:55:04 +0200

    321

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, Renderer2 } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'landsay';
+  title = 'taipan';
 
   constructor(private render : Renderer2){
 
